2a11ceb8beee6c3725106ff5d330df610e7dc6ef
etana diarta <etanadiarta@gmail.com>
2014-05-05 15:13:26 +0700

    cms

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,22 @@
+class DemoController < ApplicationController
+  
+  layout false
+
+  def index
+  end
+
+  def hello
+  	#render('index')
+  	@array = [1,2,3]
+    @id = params['id']
+    @page = params[:page].to_i
+  end
+
+  def other_hello
+  	redirect_to(:controller => 'demo', :action => 'index')
+  end
+
+  def kaskus
+  	redirect_to("http://www.kaskus.co.id/")
+  end
+end
