b92918181f78599e3e5110154ac3260d2ecaea82
etana diarta <etanadiarta@gmail.com>
2014-05-07 18:42:00 +0700

    end CRUD

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -5,7 +5,7 @@ class Subject < ActiveRecord::Base
 	scope :visible, lambda { where(:visible => true) }
 	scope :invisible, lambda { where(:visible => false) }
 	scope :sorted, lambda { order("subjects.position ASC") }
-	scope :newest_first, lambda { order("Subjects.created_at DESC")}
+	scope :newest_first, lambda { order("subjects.created_at DESC")}
 	scope :search, lambda { |query| 
 	 	where(["name LIKE ?", "%#{query}%"])
 	 }	 	
