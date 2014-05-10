35d8932769a90d285ac70d5ba271e50e6913268b
etana diarta <etanadiarta@gmail.com>
2014-05-10 17:00:43 +0700

    last name

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -14,6 +14,9 @@ class AdminUser < ActiveRecord::Base
 	# def last_name=(value)
 	# 	@last_name
 	# end
+
+	has_secure_password
+	
 	has_and_belongs_to_many :pages
 	has_many :section_edits
 	has_many :sections, :through => :section_edits
@@ -47,6 +50,14 @@ class AdminUser < ActiveRecord::Base
 
 	validate :username_is_allowed
 	#validate :no_new_users_on_saturday, :on => :create
+
+	scope :sorted, lambda { order("last_name ASC, first_name ASC") }
+
+	def name
+		"#{first_name} #{last_name}"
+		# first_name + ' ' + last_name
+		# [first_name, last_name].join(' ')
+	end
 
 	def username_is_allowed
 		if FORBIDDEN_USERNAME.include?(username)
